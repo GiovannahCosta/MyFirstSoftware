c694187c895d2f0e08ae129e929c4204dc725296
Julia Pinheiro <julia.santosspinheiro@gmail.com>
2026-01-28 11:15:38 -0300

    Adicionando o repository User com o método para o comando de inserção

---
+++ confeitaria/src/model/repositories/RepositoryUser.java
@@ -0,0 +1,40 @@
+package model.repositories;
+
+
+import model.entities.User;
+import java.sql.Connection;
+import java.sql.SQLException; 
+import java.sql.PreparedStatement; 
+
+import model.entities.User;
+
+public class RepositoryUser {
+
+	private static final String insertUser = "INSERT INTO user(firstName, lastName, email, password_hash) VALUES (?,?) "
+			+ "ON CONFLICT (email) DO NOTHING";
+
+	
+	public boolean createUser(User user) throws SQLException {
+		try (Connection conn = DBConnection.getConnection();
+			PreparedStatement stmt = conn.prepareStatement(insertUser)) { 
+			
+			stmt.setString(1, user.getFirstName());
+			stmt.setString(2, user.getLastName());
+			stmt.setString(3, user.getEmail());
+			stmt.setString(4, user.getPasswordHash());
+			
+			int rowsAffected = stmt.executeUpdate();
+			
+			return rowsAffected > 0;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	
+	private static final String insertPerson= "INSERT INTO person(firstName, lastName, email) VALUES (?, ?)"
+			+ "ON CONFLICT (email) DO NOTHING";
+
+}
